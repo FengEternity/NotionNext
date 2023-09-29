36b9515647e8278a7bc834f29ddfa222e9b82be3
Monty __L <b21060414@njupt.edu.cn>
2023-09-29 16:22:31 +0800

    update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -379,7 +379,7 @@ function TodayCard({ cRef }) {
           } cursor-pointer today-card-cover absolute w-full h-full top-0`}
           style={{
             background:
-              "url('https://bu.dusays.com/2023/03/12/640dcd3a1b146.png') no-repeat center /cover"
+              "url('https://montylee-1313931661.cos.ap-nanjing.myqcloud.com/imgs/blog/20220629231150_70886.gif') no-repeat center /cover"
           }}
         ></div>
       </div>
