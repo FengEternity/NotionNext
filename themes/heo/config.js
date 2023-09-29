c54b6511cb693c4d0f917909754ba3fcb8bf8715
Monty __L <b21060414@njupt.edu.cn>
2023-09-29 16:07:51 +0800

    update the thene named heo

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-05-14', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '温良慎独 自由从容', url: 'https://montylee.cc' },
+    { title: 'Gentle self-discipline, positive progress, clear goals, free life.', url: 'https://montylee.cc' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: 'MONTY_LEE',
+  HERO_TITLE_2: '',
+  HERO_TITLE_3: '',
+  HERO_TITLE_4: '',
+  HERO_TITLE_5: '但愿世间无早八',
+  HERO_TITLE_LINK: 'https://montylee.cc',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '光与影', url: 'https://www.montylee.cc/image?theme=heo' },
+  HERO_CATEGORY_2: { title: '二次元', url: 'https://www.montylee.cc/category/二次元?theme=heo' },
+  HERO_CATEGORY_3: { title: ' M&D', url: 'https://www.montylee.cc/category/深度学习?theme=heo' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '推荐',
